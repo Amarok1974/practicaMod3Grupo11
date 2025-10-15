64bc955d05a49ee7c7e2dab593a42eadf005ed7d
Amarok1974 <lobopa45@gmail.com>
2025-10-15 00:18:41 +0000

    Funcionando BIen

---
+++ DBconfiguration/consultador.py
@@ -0,0 +1,27 @@
+import psycopg2
+#1.- Hacer la conexión a la base de datos
+conexion = psycopg2.connect(
+    host = "localhost",
+    port = "5432",
+    database = "credenciales",
+    user = "Admin",
+    password = "p4ssw0rdDB"
+)
+
+#2.- Crear el cursor
+cursor = conexion.cursor()
+
+#3.- Ejecutar la cnsulta
+cursor.execute("SELECT * FROM usuarios")
+registros = cursor.fetchall()
+
+
+
+#4.-Mostrar los resultados
+for fila in registros:
+    print(fila) 
+
+#cerrar las conexiones
+cursor.close()
+conexion.close()
+
